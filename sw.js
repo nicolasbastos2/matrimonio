17c02d751e6e7e9a617a473da1715f4364ae840f
nicolasbastos2 <39414636+nicolasbastos2@users.noreply.github.com>
2020-02-11 22:45:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,23 @@ const cacheName = 'cache-v1';
 const resourcesToPrecache =[
     '/' ,
     'index.html' ,
-
+    '/common-css/styles.css' ,
+    '/common-css/bootstrap.css'  ,
+    '/common-css/fluidbox.min.css' ,
+    '/common-css/layerslider.css' ,
+    '/common-css/swiper.css' ,
+    '/common-js/bootstrap.js' ,
+    '/common-js/jquery.countdown.min.js' ,
+    '/common-js/jquery.fluidbox.min.js' ,
+    '/common-js/jquery-3.1.1.min.js' ,
+    '/common-js/tether.min.js' ,
 ];
-self.addEventListener('install', event =>{
-    console.log ('install event!');
+self.addEventListener('install', function (event) {
+    console.log ('Service worker install event!');
     event.waitUntil(
         caches.open(cacheName)
-        .then(cache =>{
-            return Cache.addAll(resourcesToPrecache);
+        .then(function (cache) {
+            return cache.addAll(resourcesToPrecache);
         })
     );
 });
@@ -19,17 +28,7 @@ self.addEventListener('activate', event =>{
 });
 
 self.addEventListener('fetch', event =>{
-    event.respondWith(caches.match(event.request)
-    .then(cachedResponse=> {
-        return cachedResponse || fetch(event.request);
-    })
+    event.respondWith(
+        caches.match(event.request) || fetch(event.request)
     );
-});
-
-let deferedPrompt;
-window.addEventListener('beforeinstallptompt', (e) =>{
-    e.preventDefault();
-    deferredPrompt = e;
-});
-
-
+    });
